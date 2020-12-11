40023bdca56260794386b1e2d8d2c7d1bfc1c06a
Lenox1999 <p3er.guessf3ldt@gmail.com>
2020-12-11 22:56:50 +0100

    asdf

--- config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  prefix: "/",
-  token: "NzgyOTgxMTIxMDk1ODkzMDEz.X8UF_g.ems8_7fZ-vCdeNTXnYjgwTx6cQk",
-  giphy_api_key: "qqEApAGmkNzhowTpNC5qpAG7qWF1K95g",
-  google_api_key: "AIzaSyDR2yUdhocnfO_kM4ism2dShcECJw0JOrM",
-  youtube_base_url: "https://www.youtube.com/watch?v="
-}
